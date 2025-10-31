76aef1aab3dc580f61ecba0797553a06222dceb0
patrick montenegro <patrick.m@imusic-school.com>
2025-10-31 11:06:53 +0100

    mend

--- src/core/guard/auth.guard.ts
+++ src/core/guard/auth.guard.ts
@@ -1,11 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
+import { environment } from '@environments/environment';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-
+  if (!environment.production) {
+    return true;
+  }
   // Get token from route query params
   const token = route.queryParamMap.get('token');
 
